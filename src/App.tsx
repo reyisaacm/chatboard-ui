5d2d6470f7108ed7255439a0a0d22a0c1ae1137d
Reynhart <906347@telkom.co.id>
2023-02-09 16:08:26 +0700

    preliminary layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.modules.css'
+import Button from './components/button.component'
+function App (): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>Chatboard</h1>
+        <input type="text" placeholder='Type your tag' />
+        <Button />
+      </div>
+      <div className="app__body">
+          This is the app body
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './button.modules.css'
+function Button () {
+  return (
+        <a className='button'>Apply</a>
+  )
+}
+
+export default Button
